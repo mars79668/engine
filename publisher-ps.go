9d018132904a61599ab4b4838c30eae7be06c527
dexter <178529795@qq.com>
2023-03-07 20:26:56 +0800

    修复订阅时如果无音频时首个视频帧没有被订阅的bug

--- publisher-ps.go
+++ publisher-ps.go
@@ -2,10 +2,9 @@ package engine
 
 import (
 	"github.com/pion/rtp/v2"
+	"github.com/yapingcat/gomedia/go-mpeg2"
 	"go.uber.org/zap"
-	. "m7s.live/engine/v4/codec"
 	"m7s.live/engine/v4/codec/mpegps"
-	"m7s.live/engine/v4/codec/mpegts"
 	. "m7s.live/engine/v4/track"
 	"m7s.live/engine/v4/util"
 )
@@ -17,11 +16,12 @@ type cacheItem struct {
 
 type PSPublisher struct {
 	Publisher
-	DisableReorder      bool //是否禁用rtp重排序,TCP模式下应当禁用
-	mpegps.MpegPsStream `json:"-"`
-	reorder             util.RTPReorder[*cacheItem]
-	pool                util.BytesPool
-	lastSeq             uint16
+	DisableReorder bool //是否禁用rtp重排序,TCP模式下应当禁用
+	// mpegps.MpegPsStream `json:"-"`
+	*mpegps.PSDemuxer
+	reorder util.RTPReorder[*cacheItem]
+	pool    util.BytesPool
+	lastSeq uint16
 }
 
 // 解析rtp封装 https://www.ietf.org/rfc/rfc2250.txt
@@ -29,15 +29,18 @@ func (p *PSPublisher) PushPS(rtp *rtp.Packet) {
 	if p.Stream == nil {
 		return
 	}
-	if p.EsHandler == nil {
-		p.EsHandler = p
+	if p.PSDemuxer == nil {
+		p.PSDemuxer = mpegps.NewPSDemuxer()
+		p.PSDemuxer.OnPacket = p.OnPacket
+		p.PSDemuxer.OnFrame = p.OnFrame
 		p.lastSeq = rtp.SequenceNumber - 1
 		if p.pool == nil {
 			p.pool = make(util.BytesPool, 17)
 		}
 	}
 	if p.DisableReorder {
-		p.Feed(rtp.Payload)
+		p.Input(rtp.Payload)
+		// p.Feed(rtp.Payload)
 		p.lastSeq = rtp.SequenceNumber
 	} else {
 		item := p.pool.Get(len(rtp.Payload))
@@ -50,70 +53,140 @@ func (p *PSPublisher) PushPS(rtp *rtp.Packet) {
 					p.SetLostFlag()
 				}
 			}
-			p.Feed(cacheItem.Value)
+			// p.Feed(cacheItem.Value)
+			p.Input(cacheItem.Value)
 			p.lastSeq = cacheItem.Seq
 			cacheItem.Recycle()
 		}
 	}
 }
-
-func (p *PSPublisher) ReceiveVideo(es mpegps.MpegPsEsStream) {
-	if p.VideoTrack == nil {
-		switch es.Type {
-		case mpegts.STREAM_TYPE_H264:
-			p.VideoTrack = NewH264(p.Publisher.Stream)
-		case mpegts.STREAM_TYPE_H265:
-			p.VideoTrack = NewH265(p.Publisher.Stream)
-		default:
-			//推测编码类型
-			var maybe264 H264NALUType
-			maybe264 = maybe264.Parse(es.Buffer[4])
-			switch maybe264 {
-			case NALU_Non_IDR_Picture,
-				NALU_IDR_Picture,
-				NALU_SEI,
-				NALU_SPS,
-				NALU_PPS,
-				NALU_Access_Unit_Delimiter:
-				p.VideoTrack = NewH264(p.Publisher.Stream)
-			default:
-				p.Info("maybe h265", zap.Uint8("type", maybe264.Byte()))
-				p.VideoTrack = NewH265(p.Publisher.Stream)
-			}
-		}
-	}
-	payload, pts, dts := es.Buffer, es.PTS, es.DTS
-	if dts == 0 {
-		dts = pts
-	}
-	// if binary.BigEndian.Uint32(payload) != 1 {
-	// 	panic("not annexb")
-	// }
-	p.WriteAnnexB(pts, dts, payload)
-}
-
-func (p *PSPublisher) ReceiveAudio(es mpegps.MpegPsEsStream) {
-	ts, payload := es.PTS, es.Buffer
-	if p.AudioTrack == nil {
-		switch es.Type {
-		case mpegts.STREAM_TYPE_G711A:
-			p.AudioTrack = NewG711(p.Publisher.Stream, true)
-		case mpegts.STREAM_TYPE_G711U:
-			p.AudioTrack = NewG711(p.Publisher.Stream, false)
-		case mpegts.STREAM_TYPE_AAC:
+func (p *PSPublisher) OnFrame(frame []byte, cid mpeg2.PS_STREAM_TYPE, pts uint64, dts uint64) {
+	switch cid {
+	case mpeg2.PS_STREAM_AAC:
+		if p.AudioTrack != nil {
+			p.AudioTrack.WriteADTS(uint32(pts), frame)
+		} else {
 			p.AudioTrack = NewAAC(p.Publisher.Stream)
-			p.WriteADTS(ts, payload)
-		case 0: //推测编码类型
-			if payload[0] == 0xff && payload[1]>>4 == 0xf {
-				p.AudioTrack = NewAAC(p.Publisher.Stream)
-				p.WriteADTS(ts, payload)
-			}
-		default:
-			p.Error("audio type not supported yet", zap.Uint8("type", es.Type))
 		}
-	} else if es.Type == mpegts.STREAM_TYPE_AAC {
-		p.WriteADTS(ts, payload)
-	} else {
-		p.WriteRaw(ts, payload)
+	case mpeg2.PS_STREAM_G711A:
+		if p.AudioTrack != nil {
+			p.AudioTrack.WriteRaw(uint32(pts), frame)
+		} else {
+			p.AudioTrack = NewG711(p.Publisher.Stream, true)
+		}
+	case mpeg2.PS_STREAM_G711U:
+		if p.AudioTrack != nil {
+			p.AudioTrack.WriteRaw(uint32(pts), frame)
+		} else {
+			p.AudioTrack = NewG711(p.Publisher.Stream, false)
+		}
+	case mpeg2.PS_STREAM_H264:
+		if p.VideoTrack != nil {
+			// p.WriteNalu(uint32(pts), uint32(dts), frame)
+			p.WriteAnnexB(uint32(pts), uint32(dts), frame)
+		} else {
+			p.VideoTrack = NewH264(p.Publisher.Stream)
+		}
+	case mpeg2.PS_STREAM_H265:
+		if p.VideoTrack != nil {
+			// p.WriteNalu(uint32(pts), uint32(dts), frame)
+			p.WriteAnnexB(uint32(pts), uint32(dts), frame)
+		} else {
+			p.VideoTrack = NewH265(p.Publisher.Stream)
+		}
 	}
 }
+
+func (p *PSPublisher) OnPacket(pkg mpeg2.Display, decodeResult error) {
+	// switch value := pkg.(type) {
+	// case *mpeg2.PSPackHeader:
+	// 	// fd3.WriteString("--------------PS Pack Header--------------\n")
+	// 	if decodeResult == nil {
+	// 		// value.PrettyPrint(fd3)
+	// 	} else {
+	// 		// fd3.WriteString(fmt.Sprintf("Decode Ps Packet Failed %s\n", decodeResult.Error()))
+	// 	}
+	// case *mpeg2.System_header:
+	// 	// fd3.WriteString("--------------System Header--------------\n")
+	// 	if decodeResult == nil {
+	// 		// value.PrettyPrint(fd3)
+	// 	} else {
+	// 		// fd3.WriteString(fmt.Sprintf("Decode Ps Packet Failed %s\n", decodeResult.Error()))
+	// 	}
+	// case *mpeg2.Program_stream_map:
+	// 	// fd3.WriteString("--------------------PSM-------------------\n")
+	// 	if decodeResult == nil {
+	// 		// value.PrettyPrint(fd3)
+	// 	} else {
+	// 		// fd3.WriteString(fmt.Sprintf("Decode Ps Packet Failed %s\n", decodeResult.Error()))
+	// 	}
+	// case *mpeg2.PesPacket:
+	// 	// fd3.WriteString("-------------------PES--------------------\n")
+	// 	if decodeResult == nil {
+	// 		// value.PrettyPrint(fd3)
+	// 	} else {
+	// 		// fd3.WriteString(fmt.Sprintf("Decode Ps Packet Failed %s\n", decodeResult.Error()))
+	// 	}
+	// }
+}
+
+// func (p *PSPublisher) ReceiveVideo(es mpegps.MpegPsEsStream) {
+// 	if p.VideoTrack == nil {
+// 		switch es.Type {
+// 		case mpegts.STREAM_TYPE_H264:
+// 			p.VideoTrack = NewH264(p.Publisher.Stream)
+// 		case mpegts.STREAM_TYPE_H265:
+// 			p.VideoTrack = NewH265(p.Publisher.Stream)
+// 		default:
+// 			//推测编码类型
+// 			var maybe264 H264NALUType
+// 			maybe264 = maybe264.Parse(es.Buffer[4])
+// 			switch maybe264 {
+// 			case NALU_Non_IDR_Picture,
+// 				NALU_IDR_Picture,
+// 				NALU_SEI,
+// 				NALU_SPS,
+// 				NALU_PPS,
+// 				NALU_Access_Unit_Delimiter:
+// 				p.VideoTrack = NewH264(p.Publisher.Stream)
+// 			default:
+// 				p.Info("maybe h265", zap.Uint8("type", maybe264.Byte()))
+// 				p.VideoTrack = NewH265(p.Publisher.Stream)
+// 			}
+// 		}
+// 	}
+// 	payload, pts, dts := es.Buffer, es.PTS, es.DTS
+// 	if dts == 0 {
+// 		dts = pts
+// 	}
+// 	// if binary.BigEndian.Uint32(payload) != 1 {
+// 	// 	panic("not annexb")
+// 	// }
+// 	p.WriteAnnexB(pts, dts, payload)
+// }
+
+// func (p *PSPublisher) ReceiveAudio(es mpegps.MpegPsEsStream) {
+// 	ts, payload := es.PTS, es.Buffer
+// 	if p.AudioTrack == nil {
+// 		switch es.Type {
+// 		case mpegts.STREAM_TYPE_G711A:
+// 			p.AudioTrack = NewG711(p.Publisher.Stream, true)
+// 		case mpegts.STREAM_TYPE_G711U:
+// 			p.AudioTrack = NewG711(p.Publisher.Stream, false)
+// 		case mpegts.STREAM_TYPE_AAC:
+// 			p.AudioTrack = NewAAC(p.Publisher.Stream)
+// 			p.WriteADTS(ts, payload)
+// 		case 0: //推测编码类型
+// 			if payload[0] == 0xff && payload[1]>>4 == 0xf {
+// 				p.AudioTrack = NewAAC(p.Publisher.Stream)
+// 				p.WriteADTS(ts, payload)
+// 			}
+// 		default:
+// 			p.Error("audio type not supported yet", zap.Uint8("type", es.Type))
+// 		}
+// 	} else if es.Type == mpegts.STREAM_TYPE_AAC {
+// 		p.WriteADTS(ts, payload)
+// 	} else {
+// 		p.WriteRaw(ts, payload)
+// 	}
+// }
